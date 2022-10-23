224890894e0ee75e46a8deb41351d1170855dc86
Lincoln Bacchus <lincolnbacchus@gmail.com>
2022-10-24 00:50:25 +0200

    Module_00: ex01

---
+++ CPP_Module_00/ex01/phonebook.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phonebook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: libacchu <libacchu@students.42wolfsburg    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/24 00:23:52 by libacchu          #+#    #+#             */
+/*   Updated: 2022/10/24 00:32:57 by libacchu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+
+class	Contacts
+{
+	private:
+	
+	public:	
+};
+
+class	PhoneBook
+{
+	public:
+		Contacts contact[8];
+};
+
+#endif
