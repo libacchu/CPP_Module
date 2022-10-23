d6f4d84d7711e1a3c18ae390607ab9fe923de413
Lincoln Bacchus <lincolnbacchus@gmail.com>
2022-10-24 00:50:13 +0200

    Module_00: ex00

---
+++ CPP_Module_00/ex00/megaphone.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: libacchu <libacchu@students.42wolfsburg    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/23 23:26:21 by libacchu          #+#    #+#             */
+/*   Updated: 2022/10/24 00:12:58 by libacchu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int ac, char ** av)
+{
+	if (ac == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		return(1);
+	}
+	for (int i = 1; i < ac; i++)
+	{
+		for (int j = 0; av[i][j]; j++)
+			std::cout << (char)toupper(av[i][j]);
+	}
+	std::cout << "\n";
+	return (0);
+}
