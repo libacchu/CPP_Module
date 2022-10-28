ae584b8257da763d4ea17cccb31a7ccea7b9a8bc
Lincoln Bacchus <lincolnbacchus@gmail.com>
2022-10-28 08:46:01 +0200

    Created: classes

--- CPP_Module_00/ex01/phonebook.cpp
+++ CPP_Module_00/ex01/phonebook.cpp
@@ -6,13 +6,97 @@
 /*   By: libacchu <libacchu@students.42wolfsburg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 00:23:26 by libacchu          #+#    #+#             */
-/*   Updated: 2022/10/24 00:32:47 by libacchu         ###   ########.fr       */
+/*   Updated: 2022/10/28 08:43:22 by libacchu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "phonebook.hpp"
+#include <iomanip>
 
-int	main(void)
+void Contacts::set_first_name()
 {
-		
+	std::cout << "Enter first name: ";
+	std::cin >> this->first_name;
+}
+
+void Contacts::set_last_name()
+{
+	std::cout << "Enter last name: ";
+	std::cin >> this->last_name;
+}
+
+void Contacts::set_nickname()
+{
+	std::cout << "Enter nickname: ";
+	std::cin >> this->nickname;
+}
+
+void Contacts::set_phone_nbr()
+{
+	std::cout << "Enter phonenumber: ";
+	std::cin >> this->phone_number;
+}
+
+void Contacts::set_darkest_secret()
+{
+	std::cout << "what is you darkest secret: ";
+	std::cin >> this->darkest_secret;
+}
+
+std::string Contacts::get_first_name()
+{
+	return (this->first_name);
+}
+
+std::string Contacts::get_last_name()
+{
+	return (this->last_name);
+}
+
+std::string Contacts::get_nickname()
+{
+	return (this->nickname);
+}
+
+int	Contacts::get_phone_nbr()
+{
+	return (this->phone_number);
+}
+
+std::string Contacts::get_darkest_secret()
+{
+	return (this->darkest_secret);
+}
+
+// void add(Contacts *contact)
+// {
+// 	contact[Contacts::nbr_of_contacts % 8].set_first_name();
+// 	contact[Contacts::nbr_of_contacts % 8].set_last_name();
+// 	contact[Contacts::nbr_of_contacts % 8].set_nickname();
+// 	contact[Contacts::nbr_of_contacts % 8].set_phone_nbr();
+// 	contact[Contacts::nbr_of_contacts % 8].set_darkest_secret();
+// }
+
+void	PhoneBook::print_contact_table()
+{
+	for (int i = 0; i < 45; i++)
+		std::cout << "-";
+	std::cout << std::endl;
+	
+	std::cout << std::right;
+	std::cout << "|";
+	std::cout << std::setw(10) << "INDEX";
+	std::cout << "|";
+	std::cout << std::setw(10) << "FIRST NAME";
+	std::cout << "|";
+	std::cout << std::setw(10) << "LAST NAME";
+	std::cout << "|";
+	std::cout << std::setw(10) << "NICKNAME";
+	std::cout << "|";
+	std::cout << std::endl;
+	for (int i = 0; i < 45; i++)
+		std::cout << "-";
+	std::cout << std::endl;
+	
+	
 }
--- CPP_Module_00/ex01/phonebook.hpp
+++ CPP_Module_00/ex01/phonebook.hpp
@@ -6,24 +6,59 @@
 /*   By: libacchu <libacchu@students.42wolfsburg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 00:23:52 by libacchu          #+#    #+#             */
-/*   Updated: 2022/10/24 00:32:57 by libacchu         ###   ########.fr       */
+/*   Updated: 2022/10/28 08:34:22 by libacchu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHONEBOOK_HPP
 # define PHONEBOOK_HPP
 
+# include <iostream>
+# include <string>
+# include <iomanip>
+
+#define DEFAULT "\033[0;39m"
+#define B_RED "\033[1;31m"
+#define GREEN "\033[32m"
+#define BLACK "\033[30m"
+#define GREY "\033[90m"
+#define CYAN "\033[36m"
+#define YELLOW "\033[33m"
+#define B_BLUE "\033[1;94m"
+#define MAGENTA "\033[35m"
+
 class	Contacts
 {
 	private:
-	
-	public:	
+		int			index;
+		std::string	first_name;
+		std::string	last_name;
+		std::string	nickname;
+		int			phone_number;
+		std::string	darkest_secret;
+		static int	nbr_of_contacts;
+		
+	public:
+		void		set_first_name();
+		void		set_last_name();
+		void		set_nickname();
+		void		set_phone_nbr();
+		void		set_darkest_secret();
+		std::string get_first_name();
+		std::string get_last_name();
+		std::string get_nickname();
+		int			get_phone_nbr();
+		std::string get_darkest_secret();
 };
 
 class	PhoneBook
 {
+	private:	
+		Contacts	contact[8];
 	public:
-		Contacts contact[8];
+		void		print_contact_table();
 };
 
+void	add();
+void	search();
 #endif
