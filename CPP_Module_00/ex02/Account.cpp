8082616b0e92556bb05cc5b203391c39deeba8af
libacchu <libacchu@students.42wolfsburg.de>
2022-10-30 06:44:08 +0100

    init: ex02

---
+++ CPP_Module_00/ex02/Account.cpp
@@ -0,0 +1,62 @@
+// ************************************************************************** //
+//                                                                            //
+//                Account.hpp for GlobalBanksters United                      //
+//                Created on  : Thu Nov 20 19:43:15 1989                      //
+//                Last update : Wed Jan 04 14:54:06 1992                      //
+//                Made by : Brad "Buddy" McLane <bm@gbu.com>                  //
+//                                                                            //
+// ************************************************************************** //
+
+#include "Account.hpp"
+
+static int	getNbAccounts( void ) {
+	return (t._nbAccounts);
+}
+
+static int	getTotalAmount( void ){
+	return (t._totalAmount);
+}
+
+static int	getNbDeposits( void ) {
+	return (t._totalNbDeposits);
+}
+
+static int	getNbWithdrawals( void ) {
+	return (this->_nbWithdrawals);
+}
+
+static void	displayAccountsInfos( void ) {
+
+}
+
+Account::Account( int initial_deposit ) {
+
+}
+
+Account::~Account( void ) {
+
+}
+
+void	makeDeposit( int deposit ) {
+	
+}
+
+bool	makeWithdrawal( int withdrawal ) {
+
+}
+
+int		checkAmount( void ) const {
+
+}
+
+void	displayStatus( void ) const {
+
+}
+
+static void	_displayTimestamp( void ) {
+	
+}
+
+Account::Account( void ) {
+
+}
