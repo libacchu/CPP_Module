39c6b1d9b7d4ecb19a23d5ab9c5d9cec9832f7d4
Lincoln Bacchus <lincolnbacchus@gmail.com>
2022-11-02 16:08:31 +0100

    ex02

---
+++ CPP_Module_01/ex02/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: libacchu <libacchu@students.42wolfsburg    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/02 15:48:06 by libacchu          #+#    #+#             */
+/*   Updated: 2022/11/02 16:05:42 by libacchu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(void)
+{
+	std::string		string = "HI THIS IS BRAIN";
+	std::string*	stringPTR = &string;
+	std::string&	stringREF = string;
+
+	std::cout << "The memory address of the string variable = " << &string << std::endl;
+	std::cout << "The memory address held by stringPTR = " << &stringPTR << std::endl;
+	std::cout << "The memory address held by stringREF = " << &stringREF << std::endl;
+	std::cout << std::endl;
+	std::cout << "The value of the string variable = " << string << std::endl;
+	std::cout << "The value pointed to by stringPTR = " << stringPTR << std::endl;
+	std::cout << "The value pointed to by stringREF = " << stringREF << std::endl;
+	
+}
