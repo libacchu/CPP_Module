2ca11b2220e6d06afe6e3537c303d857adbde975
Lincoln Bacchus <lincolnbacchus@gmail.com>
2022-11-08 13:22:44 +0100

    CPP_01_ex03: print value and not address

--- CPP_Module_01/ex02/main.cpp
+++ CPP_Module_01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: libacchu <libacchu@students.42wolfsburg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 15:48:06 by libacchu          #+#    #+#             */
-/*   Updated: 2022/11/07 12:56:03 by libacchu         ###   ########.fr       */
+/*   Updated: 2022/11/08 13:18:19 by libacchu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int	main(void)
 	std::cout << "The memory address held by stringREF = " << &stringREF << std::endl;
 	std::cout << std::endl;
 	std::cout << "The value of the string variable = " << string << std::endl;
-	std::cout << "The value pointed to by stringPTR = " << stringPTR << std::endl;
+	std::cout << "The value pointed to by stringPTR = " << *stringPTR << std::endl;
 	std::cout << "The value pointed to by stringREF = " << stringREF << std::endl;
 	std::cout << std::endl;
 	
