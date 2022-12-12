18e5446c6a6fd8557a810dc3d8fc33a3ed77a97e
Lincoln Bacchus <lincolnbacchus@gmail.com>
2022-12-12 13:58:40 +0100

    CPP 05: ex01

---
+++ CPP_Module_05/ex01/Form.cpp
@@ -0,0 +1,109 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Form.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: libacchu <libacchu@students.42wolfsburg    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/17 19:45:04 by libacchu          #+#    #+#             */
+/*   Updated: 2022/12/09 17:41:53 by libacchu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Form.hpp"
+
+Form::Form( void )
+: _name("unnamed form"), _signitureStatus(false), _gradeToSign(150), _gradeToExecute(150)
+{
+	std::cout << B_BLUE "Form default constructor called." DEFAULT << std::endl;
+}
+
+Form::Form(const std::string name, int gradeToSign, int gradeToExecute)
+: _name(name), _signitureStatus(false), _gradeToSign(gradeToSign), \
+_gradeToExecute(gradeToExecute)
+{
+	std::cout << B_BLUE "Form parameter constructor called." DEFAULT << std::endl;
+	if (checkGradeError( _gradeToSign ))
+		new (this) Form(name, _minGrade, gradeToExecute);
+	if (checkGradeError( _gradeToExecute ))
+		new (this) Form(name, _gradeToSign, _minGrade);
+}
+
+Form::Form( const Form & copy): _name(copy._name + "_copy"), _signitureStatus(copy._signitureStatus), \
+_gradeToSign(copy._gradeToSign), _gradeToExecute(copy._gradeToExecute)
+{
+	std::cout << B_BLUE "Form copy constructor called." DEFAULT << std::endl;
+}
+
+Form& Form::operator=( const Form & rhs ) {
+    std::cout << B_BLUE "Form copy operator called." DEFAULT << std::endl;
+	/* _name, _gradeToSign, _gradeToExecute are constant and cannot be copied*/
+	this->_signitureStatus = rhs._signitureStatus;
+    return(*this);
+}
+
+Form::~Form( void ) {
+	std::cout << B_BLUE "Form destructor called." DEFAULT << std::endl;
+}
+
+/* ------------ GETTERS ------------ */
+const std::string&	Form::getName( void ) const {
+	return ( this->_name );
+}
+
+const bool&	Form::getSignitureStatus( void ) const {
+	return ( this->_signitureStatus );
+}
+
+const int&	Form::getGradeToSign( void ) const {
+	return ( this->_gradeToSign );
+}
+const int&	Form::getGradeToExecute( void ) const {
+	return ( this->_gradeToExecute );
+}
+
+/* ------------ METHODS ------------ */
+bool	Form::signForm( Bureaucrat &bureaucrat ) {
+
+	if (bureaucrat.getGrade() <= this->_gradeToSign) {	
+		this->_signitureStatus = true;
+		std::cout << bureaucrat.getName() << " signed " << this->getName() << std::endl;
+	}
+	else
+		std::cout << bureaucrat.getName() << " couldn't signed " << this->getName() << std::endl;
+	return ( this->_signitureStatus );
+}
+
+bool Form::checkGradeError( int grade ) {
+	try {
+		if (grade < 1)
+			throw (Form::GradeTooHighException());
+		else if (grade > _minGrade)
+			throw (Form::GradeTooLowException());	
+	}
+	catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+		return (true);
+	}
+	return (false);
+}
+
+const char* Form::GradeTooHighException::what() const throw() {
+	return ("Exception: Grade too High");
+}
+
+const char* Form::GradeTooLowException::what() const throw() {
+	return ("Exception: Grade too Low");
+}
+
+std::ostream & operator<<( std::ostream & o, Form const & rhs) {
+    o << "Form: " << rhs.getName() << " is ";
+	if (rhs.getSignitureStatus() == true)
+		o << B_BLUE "signed" DEFAULT;
+	else
+		o << B_RED "not signed" DEFAULT;
+	o << ": the grade required to sign is " << rhs.getGradeToSign();
+	o << ", the grade required to execute is " << rhs.getGradeToExecute();
+	o << ".";
+	return (o);
+}
---
+++ CPP_Module_05/ex01/Form.hpp
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Form.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: libacchu <libacchu@students.42wolfsburg    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/17 19:30:30 by libacchu          #+#    #+#             */
+/*   Updated: 2022/12/09 17:09:54 by libacchu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FORM_HPP
+# define FORM_HPP
+
+# include <iostream>
+# include <string>
+# include "Bureaucrat.hpp"
+
+class Form
+{
+    private:
+        Form( void );
+        const std::string	_name;
+        bool            	_signitureStatus;
+        const int       	_gradeToSign;
+        const int       	_gradeToExecute;
+		static const int	_minGrade = 150;
+		bool				checkGradeError( int grade);
+    public:
+		Form( const std::string name, int gradeToSign, int gradeToExecute );
+        Form( const Form & copy);
+        Form& operator=( const Form & rhs );
+        ~Form();
+
+		const std::string&	getName( void ) const;
+		const bool&			getSignitureStatus( void ) const;
+		const int&			getGradeToSign( void ) const;
+		const int&			getGradeToExecute( void ) const;
+		
+		bool				signForm( Bureaucrat &bureaucrat );
+
+		class GradeTooHighException : public std::exception {
+			virtual const char* what() const throw();
+		};
+
+		class GradeTooLowException : public std::exception {
+			virtual const char* what() const throw();
+		};
+};
+
+std::ostream & operator<<(std::ostream & o, Form const & rhs);
+
+#  endif
