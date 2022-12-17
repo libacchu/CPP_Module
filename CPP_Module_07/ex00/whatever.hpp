475348eeeca757174cf1853513706a5c34c695c1
libacchu <libacchu@students.42wolfsburg.de>
2022-12-17 18:27:55 +0100

    CPP 07: ex00

---
+++ CPP_Module_07/ex00/whatever.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: libacchu <libacchu@students.42wolfsburg    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/17 18:14:11 by libacchu          #+#    #+#             */
+/*   Updated: 2022/12/17 18:27:14 by libacchu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+#include <iostream>
+#include <string>
+
+template< typename T >
+T const&	max( T const& x, T const& y ) {
+	return ( (x > y) ? x : y );
+};
+
+template< typename T >
+T const&	min( T const& x, T const& y ) {
+	return ( (x < y) ? x : y );
+}
+
+template< typename T >
+void	swap( T& x, T& y ) {
+	T tmp = x;
+	x = y;
+	y = tmp;
+}
+
+#endif
