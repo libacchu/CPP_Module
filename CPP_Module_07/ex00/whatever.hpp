f06264393d9c7d4bf2b6ac1dbeaaa69783505550
libacchu <libacchu@students.42wolfsburg.de>
2022-12-17 19:12:43 +0100

    CPP 07: ex00

--- CPP_Module_07/ex00/whatever.hpp
+++ CPP_Module_07/ex00/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: libacchu <libacchu@students.42wolfsburg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/17 18:14:11 by libacchu          #+#    #+#             */
-/*   Updated: 2022/12/17 18:27:14 by libacchu         ###   ########.fr       */
+/*   Updated: 2022/12/17 18:31:45 by libacchu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 template< typename T >
 T const&	max( T const& x, T const& y ) {
 	return ( (x > y) ? x : y );
-};
+}
 
 template< typename T >
 T const&	min( T const& x, T const& y ) {
