7a7474a9125e445b02c3d75fdf81a3b069d457d2
libacchu <libacchu@students.42wolfsburg.de>
2023-01-05 08:35:42 +0100

    update: main - added more tests

--- CPP_Module_05/ex02/main.cpp
+++ CPP_Module_05/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: libacchu <libacchu@students.42wolfsburg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 14:35:04 by libacchu          #+#    #+#             */
-/*   Updated: 2023/01/04 14:57:03 by libacchu         ###   ########.fr       */
+/*   Updated: 2023/01/05 08:31:11 by libacchu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,10 @@ int main ( void )
 		try {
 			std::cout << std::endl;
 			std::cout << form << std::endl;
+
 			form.signForm( Peter );
 			Peter.executeForm( form );
+			
 			std::cout << form << std::endl;
 			std::cout << std::endl;
 		}
@@ -41,18 +43,41 @@ int main ( void )
 		std::cout << std::endl;
 		std::cout << B_YELLOW "---------- TEST 2 ----------" DEFAULT << std::endl;
 
-		Bureaucrat	Paul( "Peter", 45 );
-		RobotomyRequestForm form( "That Guy" );
+		Bureaucrat	Paul( "Paul", 45 );
+		RobotomyRequestForm form( "James" );
 		
 		try {
 			std::cout << std::endl;
 			std::cout << form << std::endl;
+			
 			Paul.executeForm( form );
 			form.signForm( Paul );
 			Paul.executeForm( form );
 			form.execute( Paul );
 			form.execute( Paul );
 			form.execute( Paul );
+
+			std::cout << form << std::endl;
+			std::cout << std::endl;
+		}
+		catch (std::exception &e) {
+			std::cout << B_RED << e.what() << DEFAULT << std::endl;
+		}
+	}
+		{
+		std::cout << std::endl;
+		std::cout << B_YELLOW "---------- TEST 2.1 ----------" DEFAULT << std::endl;
+
+		Bureaucrat	Linda( "Linda", 50 );
+		RobotomyRequestForm form( "Robert" );
+		
+		try {
+			std::cout << std::endl;
+			std::cout << form << std::endl;
+			
+			form.signForm( Linda );
+			Linda.executeForm( form );
+
 			std::cout << form << std::endl;
 			std::cout << std::endl;
 		}
@@ -70,15 +95,37 @@ int main ( void )
 		try {
 			std::cout << std::endl;
 			std::cout << form << std::endl;
+			
 			form.signForm( John );
 			John.executeForm( form );
+
 			std::cout << form << std::endl;
 			std::cout << std::endl;
 		}
 		catch (std::exception &e) {
 			std::cout << B_RED << e.what() << DEFAULT << std::endl;
 		}
+	}
+		{
+		std::cout << std::endl;
+		std::cout << B_YELLOW "---------- TEST 3.1 ----------" DEFAULT << std::endl;
 		
+		Bureaucrat	William( "William", 26 );
+		PresidentialPardonForm form( "David" );
+		
+		try {
+			std::cout << std::endl;
+			std::cout << form << std::endl;
+			
+			form.signForm( William );
+			William.executeForm( form );
+
+			std::cout << form << std::endl;
+			std::cout << std::endl;
+		}
+		catch (std::exception &e) {
+			std::cout << B_RED << e.what() << DEFAULT << std::endl;
+		}
 	}
 	return (0);
 }
