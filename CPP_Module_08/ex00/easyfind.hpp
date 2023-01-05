ef37618ce1df9a63d072919671fdf09f92b917d8
libacchu <libacchu@students.42wolfsburg.de>
2023-01-05 16:38:18 +0100

    cpp08: ex00

---
+++ CPP_Module_08/ex00/easyfind.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: libacchu <libacchu@students.42wolfsburg    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/05 14:47:22 by libacchu          #+#    #+#             */
+/*   Updated: 2023/01/05 15:02:04 by libacchu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+# include <iostream>
+# include <string>
+
+template< typename T >
+class easyFind
+{
+	
+	
+};
+
+#endif
