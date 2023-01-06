5fdc87d808d3dab3f3d25fad0d2153ca81d2314f
libacchu <libacchu@students.42wolfsburg.de>
2023-01-06 12:16:01 +0100

    update

--- CPP_Module_08/ex00/easyfind.hpp
+++ CPP_Module_08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: libacchu <libacchu@students.42wolfsburg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 14:47:22 by libacchu          #+#    #+#             */
-/*   Updated: 2023/01/05 15:02:04 by libacchu         ###   ########.fr       */
+/*   Updated: 2023/01/06 12:14:02 by libacchu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,24 @@
 
 # include <iostream>
 # include <string>
+# include <algorithm>
+# include <iterator>
+# include <vector>
+
+# define DEFAULT "\033[0;39m"
+# define B_RED "\033[1;31m"
+# define B_BLUE "\033[1;94m"
+# define B_YELLOW "\033[1;33m"
+# define B_GREEN "\033[1;32m"
+# define B_PINK "\033[1;35m"
 
 template< typename T >
-class easyFind
+typename T::iterator easyfind( T& container, int toFind )
 {
-	
-	
-};
+	typename T::iterator result = std::find(container.begin(), container.end(), toFind);
+	if (result == container.end())
+		throw ("Element not found");
+	return (result);
+}
 
 #endif
--- CPP_Module_08/ex00/main.cpp
+++ CPP_Module_08/ex00/main.cpp
@@ -6,7 +6,50 @@
 /*   By: libacchu <libacchu@students.42wolfsburg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 14:47:25 by libacchu          #+#    #+#             */
-/*   Updated: 2023/01/05 14:47:26 by libacchu         ###   ########.fr       */
+/*   Updated: 2023/01/06 12:11:31 by libacchu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "easyfind.hpp"
+
+int main( void )
+{
+	// {
+	// 	std::vector<int> myints;
+	// 	myints.push_back(10);
+	// 	myints.push_back(30);
+	// 	myints.push_back(40);
+	// 	myints.push_back(20);
+	// 	try
+	// 	{
+	// 		std::vector<int>::iterator it = easyfind(myints, );
+	// 		std::cout << B_BLUE "Element found: " DEFAULT << *it << std::endl;
+	// 	}
+	// 	catch (...)
+	// 	{
+	// 		std::cout << B_RED "Element not found" DEFAULT << std::endl;
+	// 	}
+	// }
+	{
+		int myints[] = { 10, 20, 30, 40 };
+		// int * p;
+
+		// // p = std::find (myints, myints+4, 29);
+		// p = easyfind (myints, 29);
+		// if (p != myints+4)
+		// 	std::cout << "Element found in myints: " << *p << '\n';
+		// else
+		// 	std::cout << "Element not found in myints\n";
+
+		// using std::find with vector and iterator:
+		std::vector<int> myvector (myints,myints+4);
+		std::vector<int>::iterator it;
+
+		it = find (myvector.begin(), myvector.end(), 29);
+		if (it != myvector.end())
+			std::cout << "Element found in myvector: " << *it << '\n';
+		else
+			std::cout << "Element not found in myvector\n";
+	}
+	return (0);
+}
